e73c14af08c7d748fdb6f3f81b12e99570fdc08e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-18 17:47:34 -0500

    added form to input name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect} from "react";
 
 function App() {
-  const [displayGame, setDisplayGame] = useState(false)
-  const [displayPlayerName, setDisplayPlayerName] = useState(false)
   const [displayWelcome, setDisplayWelcome] = useState(true)
+  const [displayNameInput, setDisplayNameInput] = useState(false)
+  const [displayGame, setDisplayGame] = useState(false)
+  const [nameInput, setNameInput] = useState("")
+
+  const handleSubmitName = (event) => {
+    event.preventDefault();
+    console.log(nameInput)
+
+  }
 
   return (
     <div className="App">
       <h1>Hey there!</h1>
-      <h1>This app is a simple rock paper scissors game in React and Rails. Would you like to play?</h1>
-      <button onClick={() => {setDisplayPlayerName(true)}}>Sure!</button><button>No, thanks.</button>
+      <h1>This app is a simple rock paper scissors game. Would you like to play?</h1>
+      <button onClick={() => {setDisplayNameInput(true)}}>Sure!</button><button>No, thanks.</button>
+      {displayNameInput ? (
+        <>
+        <h1>Awesome!</h1>
+        <h1>I need to get your name first. Please enter it below:</h1>
+        <form onSubmit={handleSubmitName}>
+          <input type="text" value={nameInput} onChange={(event) => setNameInput(event.target.value)}></input>
+          <button type="submit">Save Player</button>
+        </form>
+        </>
+      ) : null}
+      <>
+        <h1>Hi, {nameInput}!</h1>
+      </>
     </div>
   );
 }
