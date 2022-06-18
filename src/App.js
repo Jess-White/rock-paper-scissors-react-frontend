cfba5c61ce924df5e1f1f68bddab20c21d49c858
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-18 17:40:34 -0500

    added welcome message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useMemo } from "react";
 
 function App() {
+  const [displayGame, setDisplayGame] = useState(false)
+  const [displayPlayerName, setDisplayPlayerName] = useState(false)
+  const [displayWelcome, setDisplayWelcome] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey there!</h1>
+      <h1>This app is a simple rock paper scissors game in React and Rails. Would you like to play?</h1>
+      <button onClick={() => {setDisplayPlayerName(true)}}>Sure!</button><button>No, thanks.</button>
     </div>
   );
 }
