0d36f954ec5290477d1a3e5c4ef8f01a29a88769
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-20 14:44:34 -0500

    cleaned up and fixed computer-play game function

--- src/App.js
+++ src/App.js
@@ -1,137 +1,177 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import WelcomeMessage from "./Components/WelcomeMessage";
+import PlayerNamePrompt from "./Components/PlayerNamePrompt";
+import GameTypePrompt from "./Components/GameTypePrompt";
+import GamePlayPrompt from "./Components/GamePlayPrompt";
+import ResultsDisplay from "./Components/ResultsDisplay";
+import NoThanksMessage from "./Components/NoThanksMessage";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import * as PlayerService from "./Services/PlayerService";
 
 function App() {
-  const [displayWelcome, setDisplayWelcome] = useState(true);
-  const [displayNameInput, setDisplayNameInput] = useState(false);
-  const [displayGame, setDisplayGame] = useState(false);
+  const [displayArgs, setDisplayArgs] = useState({
+    display: "welcomeMessage",
+    type: 0,
+  });
   const [gameType, setGameType] = useState(0);
   const [player1NameInput, setPlayer1NameInput] = useState("");
   const [player2NameInput, setPlayer2NameInput] = useState("");
-  // const [choiceArray, setChoiceArray] = useState([]);
-  // const [player1Choice, setPlayer1Choice] = useState({});
-  // const [player2Choice, setPlayer2Choice] = useState({});
-  const [gameArgs, setGameArgs] = useState({ player1: "", player2: "" });
+  const [gameArgs, setGameArgs] = useState({ player1: null, player2: null });
+  const [result, setResult] = useState("");
+  const queryClient = useQueryClient();
 
-  const handleSubmitName = (event) => {
-    event.preventDefault();
-    console.log(event);
-    console.log(event.target[0].value);
-  };
+  const ROCK_PAPER_SCISSORS = ["rock", "paper", "scissors"];
 
-  useEffect(() => {
-    playGame(gameArgs);
-  }, [gameArgs]);
-
-  const playGame = (choices) => {
-    console.log(choices);
-    if (choices.player1.length > 1 && choices.player2.length > 1) {
-      console.log("judge game!");
+  const clearAll = () => {
+    if (
+      displayArgs.display === "welcomeMessage" ||
+      displayArgs.display === "gameTypePrompt"
+    ) {
+      setGameType(0);
+      setPlayer1NameInput("");
+      setPlayer2NameInput("");
+      setGameArgs({ player1: null, player2: null });
+      setResult("");
     }
   };
 
-  return (
-    <div className="App">
-      <h1>Hey there!</h1>
-      <h1>
-        This app is a simple rock paper scissors game. Would you like to play?
-      </h1>
-      <button
-        onClick={() => {
-          setDisplayNameInput(true);
-        }}
-      >
-        Sure!
-      </button>
-      <button>No, thanks.</button>
-      {displayNameInput ? (
-        <>
-          <h1>Awesome!</h1>
-          <>
-            <h1>
-              Would you like to play a two-person game, or one person against
-              the computer?
-            </h1>
-            <button
-              onClick={() => {
-                setGameType(2);
-              }}
-            >
-              Two-person game.
-            </button>
-            <button
-              onClick={() => {
-                setGameType(1);
-              }}
-            >
-              Play against the computer
-            </button>
-          </>
+  useEffect(() => {
+    clearAll();
+  }, [displayArgs]);
 
-          <h1>I need to get your name first. Please enter it below:</h1>
-
-          <form onSubmit={handleSubmitName}>
-            <input
-              type="text"
-              value={player1NameInput}
-              onChange={(event) => setPlayer1NameInput(event.target.value)}
-            ></input>
-            <button type="submit">Save Player</button>
-          </form>
-          <form onSubmit={handleSubmitName}>
-            <input
-              type="text"
-              value={player2NameInput}
-              onChange={(event) => setPlayer2NameInput(event.target.value)}
-            ></input>
-            <button type="submit">Save Player</button>
-          </form>
-        </>
-      ) : null}
-      <>
-        <h1>Hi, {player1NameInput}!</h1>
-        <div>
-          <button
-            value={gameArgs.player1}
-            onClick={() => setGameArgs({ ...gameArgs, player1: "rock" })}
-          >
-            Rock
-          </button>
-          <button
-            value={gameArgs.player1}
-            onClick={() => setGameArgs({ ...gameArgs, player1: "paper" })}
-          >
-            Paper
-          </button>
-          <button
-            value={gameArgs.player1}
-            onClick={() => setGameArgs({ ...gameArgs, player1: "scissors" })}
-          >
-            Scissors
-          </button>
-        </div>
-        <h1>Hi, {player2NameInput}!</h1>
-        <button
-          value={gameArgs.player2}
-          onClick={() => setGameArgs({ ...gameArgs, player2: "rock" })}
-        >
-          Rock
-        </button>
-        <button
-          value={gameArgs.player2}
-          onClick={() => setGameArgs({ ...gameArgs, player2: "paper" })}
-        >
-          Paper
-        </button>
-        <button
-          value={gameArgs.player2}
-          onClick={() => setGameArgs({ ...gameArgs, player2: "scissors" })}
-        >
-          Scissors
-        </button>
-      </>
-    </div>
+  const { mutate: createPlayer } = useMutation(
+    (playerFields) => PlayerService.createPlayer(playerFields),
+    {
+      onSuccess: () => {
+        console.log("Player created!");
+        queryClient.invalidateQueries();
+      },
+    }
   );
+
+  const handleSubmitName = (newPlayerFields) => {
+    createPlayer({
+      player_name: newPlayerFields,
+    });
+    queryClient.invalidateQueries();
+    refetch();
+  };
+
+  const handleStartGame = () => {
+    setDisplayArgs({ display: "gamePlayPrompt", type: displayArgs.type });
+  };
+
+  const { data: players, refetch } = useQuery("getPlayers", () =>
+    PlayerService.getPlayers()
+  );
+
+  const { mutate: updatePlayer } = useMutation(
+    (playerId, playerFields) =>
+      PlayerService.updatePlayer(playerId, playerFields),
+    {
+      onSuccess: () => {
+        console.log("Player updated - wins added!");
+      },
+    }
+  );
+
+  useEffect(() => {
+    refetch();
+  }, [gameArgs]);
+
+  const handleAddPlayerWins = (gameResult) => {
+    queryClient.invalidateQueries();
+    refetch();
+    if (gameResult === "draw") {
+      return;
+    }
+    const winningPlayerName =
+      gameResult === "player1" ? player1NameInput : player2NameInput;
+    const winningPlayer = players.find((player) => {
+      return player.player_name === winningPlayerName;
+    });
+    return updatePlayer(winningPlayer.id, {
+      player_name: winningPlayer.player_name,
+      wins: winningPlayer.wins + 1,
+    });
+  };
+
+  useEffect(() => {
+    if (displayArgs.type === 1 && gameArgs.player1 && !gameArgs.player2) {
+      const randomSelection =
+        ROCK_PAPER_SCISSORS[(ROCK_PAPER_SCISSORS.length * Math.random()) | 0];
+      setGameArgs({ ...gameArgs, player2: randomSelection });
+    }
+    if (gameArgs.player1 && gameArgs.player2) {
+      playGame(gameArgs);
+    }
+  }, [gameArgs, gameArgs.player1, setGameArgs]);
+
+  const determineWinner = ({ player1, player2 }) => {
+    if (player1 === player2) {
+      return "draw";
+    } else if (
+      (player1 === "paper" && player2 === "rock") ||
+      (player1 === "rock" && player2 === "scissors") ||
+      (player1 === "scissors" && player2 === "paper")
+    ) {
+      return "player1";
+    } else {
+      return "player2";
+    }
+  };
+
+  const playGame = async (choices) => {
+    const newResult = determineWinner(choices);
+    await handleAddPlayerWins(newResult);
+    setResult(newResult);
+    setDisplayArgs({ display: "resultsDisplay", type: 0 });
+  };
+
+  const GameDisplay = () => {
+    const { display, type } = displayArgs;
+    if (display === "welcomeMessage") {
+      return <WelcomeMessage setDisplayArgs={setDisplayArgs} />;
+    } else if (displayArgs.display === "gameTypePrompt") {
+      return <GameTypePrompt setDisplayArgs={setDisplayArgs} />;
+    } else if (display === "playerNamePrompt") {
+      return (
+        <PlayerNamePrompt
+          gameType={type}
+          setPlayer1NameInput={setPlayer1NameInput}
+          setPlayer2NameInput={setPlayer2NameInput}
+          player1NameInput={player1NameInput}
+          player2NameInput={player2NameInput}
+          handleSubmitName={handleSubmitName}
+          handleStartGame={handleStartGame}
+        />
+      );
+    } else if (display === "gamePlayPrompt") {
+      return (
+        <GamePlayPrompt
+          gameType={type}
+          setGameArgs={setGameArgs}
+          gameArgs={gameArgs}
+          player1NameInput={player1NameInput}
+          player2NameInput={player2NameInput}
+        />
+      );
+    } else if (display === "resultsDisplay") {
+      return (
+        <ResultsDisplay
+          setDisplayArgs={setDisplayArgs}
+          result={result}
+          player1NameInput={player1NameInput}
+          player2NameInput={player2NameInput}
+        />
+      );
+    } else if (display === "noThanksMessage") {
+      return <NoThanksMessage setDisplayArgs={setDisplayArgs} />;
+    }
+  };
+
+  return <div className="App">{GameDisplay()}</div>;
 }
 
 export default App;
